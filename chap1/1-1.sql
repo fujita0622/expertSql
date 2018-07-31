f4b0895856857b6ded3677a959355582236bf3b5
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-07-31 21:25:32 +0900

    達人に学ぶSQL徹底指南書 演習問題 1-① push

--- chap1/1-1.sql
+++ chap1/1-1.sql
@@ -28,36 +28,50 @@ greatestsテーブル
 
 [回答]
 ●問1
+-- keyとxとyを比較した最大値を取得
 SELECT 
   key,
 CASE 
-  -- 取得条件
+  -- xとyを比較した最大値の取得条件
   -- 条件1
-  -- xがより大きい場合,xのカラムを取得
+  -- xがyより大きい場合,xのカラムを取得
   WHEN x > y THEN x
   -- 条件2
-  -- xがより大きい場合,xのカラムを取得
+  -- yがxより大きい場合,yのカラムを取得
   WHEN x < y THEN y
--- 条件2
 -- 上記条件に該当しない場合
 -- xのカラムを取得 
 ELSE 
   x
+-- xとyを比較した最大値の列名に greatest と命名
 END AS greatest
+-- greatestsテーブルから取得
 FROM
   greatests
 ;
 
 ●問2
+-- keyとx,y,zの中の最大値を取得
 SELECT
   key,
+  -- x,y,zの中の最大値の取得条件
 CASE 
-  WHEN x > y THEN CASE WHEN x > z THEN x ELSE x END
-  WHEN y > x THEN CASE WHEN y > z THEN y ELSE x END
-  WHEN z > x THEN CASE WHEN z > y THEN z ELSE x END
+  -- 条件1
+  -- xがyとzより大きい場合,xのカラムを取得
+  WHEN x > y AND x > z THEN x
+  -- 条件2
+  -- yがxとzより大きい場合,yのカラムを取得
+  WHEN y > x AND y > z THEN y
+  -- 条件3
+  -- zがxとyより大きい場合,zのカラムを取得
+  WHEN z > x AND z > y THEN z
+-- 上記条件に該当しない場合
+-- xのカラムを取得 
 ELSE 
   x
+-- x,y,zの中の最大値の列名に greatest と命名
 END AS greatest
+-- greatestsテーブルから取得
 FROM
   greatests
 ;
@@ -82,4 +96,25 @@ export_sql-# ;
  B   |        5
  C   |        7
  D   |        3
+(4 rows)
+
+●問2
+export_sql=# SELECT
+export_sql-#   key,
+export_sql-# CASE 
+export_sql-#   WHEN x > y AND x > z THEN x
+export_sql-#   WHEN y > x AND y > z THEN y
+export_sql-#   WHEN z > x AND z > y THEN z
+export_sql-# ELSE 
+export_sql-#   x
+export_sql-# END AS greatest
+export_sql-# FROM
+export_sql-#   greatests
+export_sql-# ;
+ key | greatest 
+-----+----------
+ A   |        3
+ B   |        5
+ C   |        7
+ D   |        8
 (4 rows)
